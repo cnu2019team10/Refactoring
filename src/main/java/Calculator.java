1493d0a908f7c5d80f426e8c41cf9d0a656bedc2
LeeByungJin <hit4512@gmail.com>
2019-05-08 20:12:44 +0900

    refactoring base code

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public double plus(int x, int y) {
+        return x + y;
+    }
+
+    public double minus(int x, int y) {
+        return x - y;
+    }
+
+    public double mul(int x, int y) {
+        return x * y;
+    }
+
+    public double div(int x, int y) {
+        return x / y;
+    }
+}
