381188d953d7143dfbbb7a19371b5261788bd559
sglee487 <sglee487@gmail.com>
2019-05-14 00:23:56 +0900

    final master before pull request merge

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     public double plus(int x, int y) {
         return x + y;
@@ -15,22 +17,64 @@ public class Calculator {
         return x / y;
     }
 
-    // https://sourcemaking.com/refactoring/inline-temp
-    // inline temp
-    public double pow(int x, int y) {
-        double double_x = (double)x;
-        double double_y = (double)y;
-        double result;
 
-        result = Math.pow(double_x,double_y);
+    public int factorial(int x) {
+        int result = 1;
+
+        for (int i = x; i >= 1; i--) {
+            result = result * i;
+        }
 
         return result;
     }
 
-    //    이걸로 바꾸기 가능
-    //    public double pow(int x, int y) {
-    //        return Math.pow((double)x,(double)y);
-    //    }
+    public int factorialAfter(int x) {
+        if (x <= 1)
+            return x;
+        else
+            return factorialAfter(x - 1) * x;
+    }
 
+
+    public double sqrt(int x) {
+        double want_sqrt = Math.sqrt(x);
+        return want_sqrt;
+    }
+
+
+    public double pow(int x, int y) {
+        double double_x = (double) x;
+        double double_y = (double) y;
+        double result;
+
+        result = Math.pow(double_x, double_y);
+
+        return result;
+    }
+
+    public double powAfter(int x, int y) {
+        return Math.pow((double) x, (double) y);
+    }
+
+    public double CircleArea(int x) {
+        return x * x * 3.14;
+    }
+
+    static final double PI_Circle = 3.14;
+
+    public double CircleAreaAfter(int x) {
+        return x * x * PI_Circle;
+    }
+
+    public int randomDice() {
+        double dice = (Math.random() * 6);
+        int dice_ = (int) dice;
+        return dice_;
+    }
+
+    public int RectangleArea(int x, int y) {
+        int area = (int) mul(x, y);
+        return area;
+    }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,36 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
     @Test
+    public void IsCorrectFactorial() {
+        Calculator calculator = new Calculator();
+        int result = calculator.factorial(4);
+        int result_ = calculator.factorialAfter(4);
+
+        assertTrue(result == 24);
+        assertTrue(result_ == result);
+    }
+
+    @Test
     public void CalculatorPowTest() {
         Calculator cal = new Calculator();
-        double result = cal.pow(4,2);
+        double result = cal.pow(4, 2);
+        double result_ = cal.powAfter(4, 2);
         assertTrue(result == 16);
+        assertTrue(result_ == result);
+    }
+
+    @Test
+    public void CalculateCircleRateTest() {
+        Calculator cal = new Calculator();
+        double CircleRate_result = cal.CircleArea(4);
+        double CircleRate_result_ = cal.CircleAreaAfter(4);
+
+        assertTrue(CircleRate_result == 50.24);
+        assertTrue(CircleRate_result_ == CircleRate_result);
     }
 
 }
